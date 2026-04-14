c2a6ff095963a08834accf0f63f03c617e2567d8
Dominik Tröndle <domi.troendle@gmail.com>
2026-04-14 22:50:28 +0200

    adjusted build paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/memory/'
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        settings: 'pages/settings.html',
+        game: 'pages/game.html',
+        gameOver: 'pages/game-over.html',
+        gameEnded: 'pages/game-ended.html'
+      }
+    }
+  }
 });
