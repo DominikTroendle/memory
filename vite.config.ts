3c3b88adde2e8bf6ddbbef2617259344ab6207f6
Dominik Tröndle <domi.troendle@gmail.com>
2026-04-17 12:06:03 +0200

    adjusted paths

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-        settings: 'pages/settings.html',
-        game: 'pages/game.html',
-        gameOver: 'pages/game-over.html',
-        gameEnded: 'pages/game-ended.html'
+        settings: 'src/pages/settings.html',
+        game: 'src/pages/game.html',
+        gameOver: 'src/pages/game-over.html',
+        gameEnded: 'src/pages/game-ended.html'
       }
     }
   }
